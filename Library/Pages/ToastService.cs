e3dc1729fe1f6deee9afee853725663f1905cc86
MyintZaw09 <myintzawoo.mst@email.com>
2026-07-11 20:24:18 +0630

    Initial commit: Library Management System

---
+++ Library/Pages/ToastService.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Pages
+{
+    using System;
+
+    public class ToastService
+    {
+        public event Action<string, string, string>? OnShow;
+
+        // Toast အမျိုးအစားအလိုက် ခေါ်ယူမည့် မက်သတ်များ
+        public void ShowSuccess(string message, string title = "Success") => OnShow?.Invoke(message, title, "success");
+        public void ShowError(string message, string title = "Error") => OnShow?.Invoke(message, title, "danger");
+        public void ShowWarning(string message, string title = "Warning") => OnShow?.Invoke(message, title, "warning");
+    }
+}
